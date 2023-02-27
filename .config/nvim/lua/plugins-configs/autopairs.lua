02c6e880d98936160e5de9c10dea47eb69c5ee9c
Jacopown <jacopomehi@gmail.com>
2023-02-28 00:30:28 +0100

    removed an autopair that slowed down nvim

--- .config/nvim/lua/plugins-configs/autopairs.lua
+++ .config/nvim/lua/plugins-configs/autopairs.lua
@@ -22,30 +22,6 @@ autopairs.setup({
     },
 })
 
--- Add spaces between parentheses
-local brackets = { { '(', ')' }, { '[', ']' }, { '{', '}' } }
-autopairs.add_rules {
-  Rule(' ', ' ')
-    :with_pair(function (opts)
-      local pair = opts.line:sub(opts.col - 1, opts.col)
-      return vim.tbl_contains({
-        brackets[1][1]..brackets[1][2],
-        brackets[2][1]..brackets[2][2],
-        brackets[3][1]..brackets[3][2],
-      }, pair)
-    end)
-}
-for _,bracket in pairs(brackets) do
-  autopairs.add_rules {
-    Rule(bracket[1]..' ', ' '..bracket[2])
-      :with_pair(function() return false end)
-      :with_move(function(opts)
-        return opts.prev_char:match('.%'..bracket[2]) ~= nil
-      end)
-      :use_key(bracket[2])
-  }
-end
-
 -- Arrow key on javascript
 autopairs.add_rules {
   Rule('%(.*%)%s*%=>$', ' {  }', { 'typescript', 'typescriptreact', 'javascript' })
