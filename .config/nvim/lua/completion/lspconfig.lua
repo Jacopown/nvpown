8f59bc919cda39a40855baa53a28c4585f5f8076
Jacopown <jacopomehi@gmail.com>
2023-02-28 19:48:33 +0100

    moved some capabilities definition from lspconfig to lsputil and defined lsp keymaps in lsputils

--- .config/nvim/lua/completion/lspconfig.lua
+++ .config/nvim/lua/completion/lspconfig.lua
@@ -3,14 +3,14 @@ if not mason_lspconfig_ok then
 	vim.notify("There was a problem while requiring mason-lspconfig plugin")
 end
 
-local cmp_nvim_lsp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-if not cmp_nvim_lsp_ok then
-	vim.notify("There was a problem while requiring cmp_nvim_lsp plugin")
-end
-
 local lspconfig_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_ok then
 	vim.notify("There was a problem while requiring lspconfig plugin")
+end
+
+local lsputils_ok, lsputils = pcall(require, "completion.lsputils")
+if not lsputils_ok then
+	vim.notify("There was a problem while requiring lsputils")
 end
 
 local servers = { 'lua_ls', 'vimls' }
@@ -19,12 +19,10 @@ mason_lspconfig.setup {
     ensure_installed = servers,
 }
 
-local capabilities = cmp_nvim_lsp.default_capabilities()
-
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    -- on_attach = my_custom_on_attach,
-    capabilities = capabilities,
+    on_attach = lsputils.on_attach,
+    capabilities = lsputils.capabilities,
   }
 end
---
+++ .config/nvim/lua/completion/lsputils.lua
@@ -0,0 +1,30 @@
+local M = {}
+
+local cmp_nvim_lsp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not cmp_nvim_lsp_ok then
+	vim.notify("There was a problem while requiring cmp_nvim_lsp plugin")
+end
+
+local function lsp_keymaps(bufnr)
+	local opts = { noremap = true, silent = true }
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	vim.keymap.set("n", "gI", vim.lsp.buf.implementation, opts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+	vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+	vim.keymap.set("n", "<leader>lf", vim.lsp.buf.formatting, opts)
+	vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, opts)
+	vim.keymap.set("n", "<leader>lj", vim.diagnostic.goto_next, opts)
+	vim.keymap.set("n", "<leader>lk", vim.diagnostic.goto_prev, opts)
+	vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
+	vim.keymap.set("n", "<leader>ls", vim.lsp.buf.signature_help, opts)
+	vim.keymap.set("n", "<leader>lq", vim.diagnostic.setloclist, opts)
+end
+
+M.capabilities = cmp_nvim_lsp.default_capabilities()
+M.on_attach = function (client, bufnr)
+  lsp_keymaps(bufnr)
+end
+
+return M
