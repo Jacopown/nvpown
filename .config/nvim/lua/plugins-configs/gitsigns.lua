12453655392910234c98f000fe63555e1b7198da
Jacopown <jacopomehi@gmail.com>
2023-03-17 20:44:20 +0100

    added protected call

--- .config/nvim/lua/plugins-configs/gitsigns.lua
+++ .config/nvim/lua/plugins-configs/gitsigns.lua
@@ -1,1 +1,6 @@
-require('gitsigns').setup()
+local gitsigns_ok, gitsigns = pcall(require, 'gitsigns')
+if not gitsigns_ok then
+	vim.notify("There was a problem while requiring gitsigns plugin")
+end
+
+gitsigns.setup()
