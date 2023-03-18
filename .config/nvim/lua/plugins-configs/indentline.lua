3a0ca9495df2be5f5b43e1decfae81ca172b231a
Jacopown <jacopomehi@gmail.com>
2023-03-18 14:44:26 +0100

    removed indentline

--- .config/nvim/lua/plugins-configs/indentline.lua
+++
@@ -1,6 +0,0 @@
-local indentline_ok, indentline = pcall(require, 'indent_blankline')
-if not indentline_ok then
-	vim.notify("There was a problem while requiring indent_blankline plugin")
-end
-
-indentline.setup ({})
