f8fb1c05313ebb2e4fd9f607021fd8df7b2f5378
Jacopown <jacopomehi@gmail.com>
2023-03-29 23:05:16 +0200

    added relative numbering

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -26,6 +26,7 @@ local options = {
 	shiftwidth = 2,                           -- the number of spaces inserted for each indentation
 	tabstop = 2,                              -- insert 2 spaces for a tab
 	number = true,                            -- set numbered lines
+  relativenumber = true,
 	laststatus = 3,
 	showcmd = false,
 	ruler = false,
