fc1ca03e92c6b458ed15c4bcaf0d408277e74d01
Jacopown <jacopomehi@gmail.com>
2023-04-14 23:46:33 +0200

    deleted old plugin's config files

--- .config/nvim/lua/plugins-configs/comment.lua
+++
@@ -1,7 +0,0 @@
-local comment_ok, comment = pcall(require, 'Comment')
-if not comment_ok then
-	vim.notify("There was a problem while requiring comment plugin")
-end
-
-comment.setup({})
-
--- .config/nvim/lua/plugins-configs/telescope.lua
+++
@@ -1,6 +0,0 @@
-local telescope_ok, telescope = pcall(require, "telescope")
-if not telescope_ok then
-	vim.notify("There was a problem while requiring telescope plugin")
-end
-
-telescope.load_extension('fzf')
