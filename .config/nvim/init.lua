50c433da6fe46eb9499dc98d7da47949459d2366
Jacopown <jacopomehi@gmail.com>
2023-04-15 00:08:35 +0200

    init port for Lazy compatibility

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,8 @@
+local settings_ok, _ = pcall(require, "settings")
+if not settings_ok then
+	vim.notify("There was a problem while requiring settings.lua")
+end
+
 local keymaps_ok, _ = pcall(require, "keymaps")
 if not keymaps_ok then
 	vim.notify("There was a problem while requiring keymaps.lua")
@@ -19,28 +24,3 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
 
 vim.cmd[[colorscheme nordfox]]
-
-local settings_ok, _ = pcall(require, "settings")
-if not settings_ok then
-	vim.notify("There was a problem while requiring settings.lua")
-end
-
-local autocommands_ok, _ = pcall(require, "autocommands")
-if not autocommands_ok then
-	vim.notify("There was a problem while requiring autocommands.lua")
-end
-
--- local plugins_ok, _ = pcall(require, "plugins")
--- if not plugins_ok then
---	vim.notify("There was a problem while requiring plugins.lua")
--- end
-
--- local plugins_conf_ok, _ = pcall(require, "plugins-configs")
--- if not plugins_conf_ok then
---	vim.notify("There was a problem while requiring plugins config folder")
--- end
-
--- local cmp_folder_ok, _ = pcall(require, "completion")
--- if not cmp_folder_ok then
---	vim.notify("There was a problem while requiring cmp folder")
--- end
