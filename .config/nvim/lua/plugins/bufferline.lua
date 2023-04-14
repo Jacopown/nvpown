7db1eac2c2a6209d3bc84296ab20c4ee100d64db
Jacopown <jacopomehi@gmail.com>
2023-04-14 23:53:49 +0200

    config file port for bufferline

---
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,27 @@
+return {
+  "akinsho/bufferline.nvim",
+  pin = true,
+  event = 'VeryLazy',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    'famiu/bufdelete.nvim',
+  },
+  opts = {
+    options = {
+      close_command = "Bdelete! %d",       -- can be a string | function, see "Mouse actions"
+      right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+      tab_size = 30,
+      diagnostics = 'nvim_lsp',
+      diagnostics_indicator = function(_, _, diagnostics_dict)
+        local s = " "
+        for e, n in pairs(diagnostics_dict) do
+          local sym = e == "error" and "  "
+            or (e == "warning" and "  " or "  " )
+          s = s .. n .. sym
+        end
+        return s
+      end,
+      offsets = {{ filetype = "neo-tree", text = "", separator = true }},
+    },
+  },
+}
