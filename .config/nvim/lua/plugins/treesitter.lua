e55772119378f279e8920dd4b20235a68e669f57
Jacopown <jacopomehi@gmail.com>
2023-04-15 22:53:28 +0200

    removed html from treesitter becouse causing bad highlights

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,14 +4,15 @@ return {
   dependencies = {
     'windwp/nvim-ts-autotag',
     'HiPhish/nvim-ts-rainbow2',
+    'nvim-treesitter/playground',
   },
   build = function()
       require("nvim-treesitter.install").update({ with_sync = true })
   end,
   opts = {
     auto_install = true,
-    ensure_installed = { 'lua', 'vim', 'c' }, -- one of "all" or a list of languages
-    ignore_install = { "" }, -- List of parsers to ignore installing
+    ensure_installed = { 'lua', 'vim', 'c', 'query' }, -- one of "all" or a list of languages
+    ignore_install = { 'html' }, -- List of parsers to ignore installing
     highlight = {
       enable = true, -- false will disable the whole extension
       disable = { "" }, -- list of language that will be disabled
