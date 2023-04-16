133335731536e55e519b9ff8d0deb5b120fc350a
Jacopown <jacopomehi@gmail.com>
2023-04-16 03:06:38 +0200

    added color highlight for actual context

--- .config/nvim/lua/plugins/indentline.lua
+++ .config/nvim/lua/plugins/indentline.lua
@@ -1,9 +1,15 @@
 return {
   {
     'lukas-reineke/indent-blankline.nvim',
-    config = function()
-      vim.cmd [[highlight IndentBlanklineChar guifg=#444c5e gui=nocombine]]
-    end,
     -- pin = true,
+    opts = {
+      show_current_context = true,
+      show_current_context_start = true,
+      use_treesitter = true,
+    },
+    config = function(_, opts)
+      vim.cmd [[highlight IndentBlanklineChar guifg=#444c5e gui=nocombine]]
+      require("indent_blankline").setup(opts)
+    end,
   },
 }
