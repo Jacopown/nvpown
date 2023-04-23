9d8e4daf21ed68cd54cca39b9a682c99027a7337
Jacopown <jacopomehi@gmail.com>
2023-04-23 14:41:53 +0200

    configs rework

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,21 +4,15 @@ return {
     pin = true,
     dependencies = {
       'nvim-lua/plenary.nvim',
-      'nvim-telescope/telescope-fzf-native.nvim'
+      { 'nvim-telescope/telescope-fzf-native.nvim'--[[ , pin = true ]], build = 'make' }
     },
     ft = 'alpha',
-  keys = {
-    {[[<leader>ff]], ":Telescope find_files<CR>", desc = 'Telescope Find Files' },
-    {[[<leader>fw]], ":Telescope live_grep<CR>", desc = 'Telescope Live Grep' },
-  },
+    keys = {
+      {[[<leader>ff]], ":Telescope find_files<CR>", desc = 'Telescope Find Files' },
+      {[[<leader>fw]], ":Telescope live_grep<CR>", desc = 'Telescope Live Grep' },
+    },
     config = function()
       require('telescope').load_extension('fzf')
     end,
-  },
-  {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    pin = true,
-    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
-    lazy = true,
   },
 }
