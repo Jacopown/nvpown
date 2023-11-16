d1d1404d75842ef16c72d109b79f8ddd52fd59b9
Jacopown <jacopomehi@gmail.com>
2023-11-16 01:07:19 +0100

    ported configs from old version 2 to new version 3 of indent blank line plugin

--- .config/nvim/lua/plugins/indentline.lua
+++ .config/nvim/lua/plugins/indentline.lua
@@ -1,15 +1,16 @@
 return {
   {
     'lukas-reineke/indent-blankline.nvim',
-    -- pin = true,
+    pin = true,
+    main = "ibl",
     opts = {
-      show_current_context = true,
-      show_current_context_start = true,
-      use_treesitter = true,
+      -- show_current_context = true,
+      -- show_current_context_start = true,
+      -- use_treesitter = true,
     },
     config = function(_, opts)
-      vim.cmd [[highlight IndentBlanklineChar guifg=#444c5e gui=nocombine]]
-      require("indent_blankline").setup(opts)
+      -- vim.cmd [[highlight IndentBlanklineChar guifg=#444c5e gui=nocombine]]
+      require("ibl").setup(opts)
     end,
   },
 }
