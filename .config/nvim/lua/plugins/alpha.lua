34125faa3d3d7d1cfd27cba7ebf0848aa8b67250
Jacopown <jacopomehi@gmail.com>
2024-02-27 09:41:23 +0100

    added obsidian shortcut in alpha

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -20,6 +20,7 @@ return {
     dashboard.section.buttons.val = {
       dashboard.button("b", "󱏒 " .. " File Browser", ":Telescope file_browser <CR>"),
       dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+      dashboard.button("n", "󰠮 " .. " Obsidian", ":Telescope file_browser path=~/Repos/zettelkasten/zettels <CR>"),
       -- dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
       dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
       -- dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
