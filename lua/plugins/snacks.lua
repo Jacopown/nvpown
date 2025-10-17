f5878954e7124da8c0a8c7e56ea81b9a62a91b31
jacopown <jacopomehi@gmail.com>
2025-10-17 16:57:59 +0200

    bho

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,16 +1,16 @@
 return {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  ---@type snacks.Config
-  opts = {
-    --TODO: customize dashboard
-    dashboard = {
-      sections = {
-        {
-          text = {
-            {
-              [[
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		--TODO: customize dashboard
+		dashboard = {
+			sections = {
+				{
+					text = {
+						{
+							[[
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⢀⢄⠀⠀⡴⠁⠈⡆⠀⢀⡤⡀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠢⣄⠀⠀⡇⠀⡕⠀⢸⠀⢠⠃⠀⢮⠀⠹⠀⠀⣠⢾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -39,67 +39,70 @@ return {
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢤⡀⢀⠔⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠱⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ]],
-              hl = "String",
-            },
-          },
-        },
-        { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-        { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-        { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-        { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-        {
-          icon = " ",
-          key = "c",
-          desc = "Config",
-          action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-        },
-        { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-        { icon = " ", key = "q", desc = "Quit", action = ":qa", padding = 1 },
-        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-        { section = "startup" },
-      },
-    },
-    explorer = {
-      replace_netrw = true, -- Replace netrw with the snacks explorer
-    },
-    image = {},
-    indent = {},
-    input = {},
-    picker = {
-      sources = {
-        explorer = {
-          layout = { preset = "default", preview = true },
-        },
-      },
-    },
-    quickfile = {},
-    --TODO: add border to teminal
-    terminal = { win = { style = "float" } },
-    statuscolumn = {},
-    words = {},
-    zen = {},
-  },
-  keys = {
-    {
-      "<leader>e",
-      function()
-        Snacks.picker.explorer()
-      end,
-      desc = "File Explorer",
-    },
-    {
-      "<leader>lg",
-      function()
-        Snacks.lazygit.open()
-      end,
-      desc = "Open Lazygit",
-    },
-    {
-      "<leader>tt",
-      function()
-        Snacks.terminal.toggle()
-      end,
-      desc = "Toggle terminal",
-    },
-  },
+							hl = "String",
+						},
+					},
+				},
+				{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+				{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+				{ icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+				{ icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+				{
+					icon = " ",
+					key = "c",
+					desc = "Config",
+					action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+				},
+				{ icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+				{ icon = " ", key = "q", desc = "Quit", action = ":qa", padding = 1 },
+				{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+				{ section = "startup" },
+			},
+		},
+		explorer = {
+			replace_netrw = true, -- Replace netrw with the snacks explorer
+			auto_close = false,
+			focus = "list",
+		},
+		image = {},
+		indent = {},
+		input = {},
+		picker = {
+			sources = {
+				explorer = {
+					layout = { preset = "default", preview = true },
+          jump = { close = false }
+				},
+			},
+		},
+		quickfile = {},
+		--TODO: add border to teminal
+		terminal = { win = { style = "float" } },
+		statuscolumn = {},
+		words = {},
+		zen = {},
+	},
+	keys = {
+		{
+			"<leader>e",
+			function()
+				Snacks.picker.explorer()
+			end,
+			desc = "File Explorer",
+		},
+		{
+			"<leader>lg",
+			function()
+				Snacks.lazygit.open()
+			end,
+			desc = "Open Lazygit",
+		},
+		{
+			"<leader>tt",
+			function()
+				Snacks.terminal.toggle()
+			end,
+			desc = "Toggle terminal",
+		},
+	},
 }
