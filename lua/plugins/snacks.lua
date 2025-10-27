431463e048a69a981040e60e09aa646a25a0d686
jacopown <jacopomehi@gmail.com>
2025-10-27 19:25:57 +0100

    started working on a better snacks setup

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -43,15 +43,15 @@ return {
 						},
 					},
 				},
-				{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+				{ icon = " ", key = "f", desc = "Find File", action = function() Snacks.dashboard.pick('files') end },
 				{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-				{ icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-				{ icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+				-- { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+				{ icon = " ", key = "r", desc = "Recent Files", action = function() Snacks.picker.recent()end, },
 				{
 					icon = " ",
 					key = "c",
 					desc = "Config",
-					action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          action = function () Snacks.picker.files({cwd = vim.fn.stdpath('config')})end,
 				},
 				{ icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
 				{ icon = " ", key = "q", desc = "Quit", action = ":qa", padding = 1 },
@@ -59,11 +59,7 @@ return {
 				{ section = "startup" },
 			},
 		},
-		explorer = {
-			replace_netrw = true, -- Replace netrw with the snacks explorer
-			auto_close = false,
-			focus = "list",
-		},
+		explorer = {},
 		image = {},
 		indent = {},
 		input = {},
@@ -71,7 +67,6 @@ return {
 			sources = {
 				explorer = {
 					layout = { preset = "default", preview = true },
-          jump = { close = false }
 				},
 			},
 		},
