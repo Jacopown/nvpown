28875be37c629cf7762b382aceae78851963581f
jacopown <jacopomehi@gmail.com>
2025-11-16 23:29:42 +0100

    let's use buf, win and tabs the vim way

--- lua/plugins/bars.lua
+++ lua/plugins/bars.lua
@@ -5,16 +5,16 @@ return {
     lazy = false,
     config = true,
   },
-  {
-    'akinsho/bufferline.nvim',
-    lazy = false,
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-      options = {
-        diagnostics = "nvim_lsp",
-        close_icon = '󰅖',
-      },
-    },
-    config = true,
-  },
+  -- {
+  --   'akinsho/bufferline.nvim',
+  --   lazy = false,
+  --   dependencies = { 'nvim-tree/nvim-web-devicons' },
+  --   opts = {
+  --     options = {
+  --       diagnostics = "nvim_lsp",
+  --       close_icon = '󰅖',
+  --     },
+  --   },
+  --   config = true,
+  -- },
 }
