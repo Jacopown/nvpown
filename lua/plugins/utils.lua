0cbfc63378a1723a5445a307f4c5c0576e8bbab4
jacopown <jacopomehi@gmail.com>
2025-11-24 08:19:44 +0100

    added undotree plugin

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -26,4 +26,10 @@ return {
 			},
 		},
 	},
+  {
+  "jiaoshijie/undotree",
+  keys = { -- load the plugin only when using it's keybinding:
+    { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+  },
+  }
 }
