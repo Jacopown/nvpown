c85aee3d0b8fe985ff88bbaeb99ba2ffc09d1fe9
jacopown <jacopomehi@gmail.com>
2026-03-27 08:31:54 +0100

    added search word keybind

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -17,11 +17,15 @@ return {
   },
   keys = {
     { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+    { "<leader>fw", function() Snacks.picker.grep() end, desc = "Find Word" },
+
     { "<leader>fh", function() Snacks.picker.help() end, desc = "Help Pages" },
+
     { "<leader>fd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
     { "<leader>fD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
-    { "<leader>z", function() Snacks.zen() end, desc = "Toggle Zen Mode" },
     { "gr", function() Snacks.picker.lsp_references() end, desc = "Find References" },
+
+    { "<leader>z", function() Snacks.zen() end, desc = "Toggle Zen Mode" },
     { "<S-q>", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
   },
 }
