29faa57975dabbd597d173812a5eb77a155cb566
jacopown <jacopomehi@gmail.com>
2026-03-30 21:39:58 +0200

    made python lsp diagnostics less invasive

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -63,6 +63,19 @@ return {
 				},
 				basedpyright = {
 					settings = {
+            basedpyright = {
+              analysis = {
+                typeCheckingMode = "standard",
+                -- typeCheckingMode = "all",
+                diagnosticSeverityOverrides = {
+                  -- reportUnknownParameterType = "none",
+                  -- reportMissingParameterType = "none",
+                  -- reportUnknownArgumentType = "none",
+                  -- reportUnknownVariableType = "none",
+                  -- reportUnknownMemberType = "none",
+                },
+              },
+            },
             python = {
               venvPath = "~/micromamba/envs/"
               -- venvPath = "~/mamba/envs/"
